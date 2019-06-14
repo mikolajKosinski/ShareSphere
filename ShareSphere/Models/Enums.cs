268b5e9b8dcea8d5dfe0b67c41f3af711293da04
mikolaj <m.kosinski.1988@gmail.com>
2019-06-14 06:05:52 +0200

    1
    1

---
+++ ShareSphere/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareSphere.Models
+{
+    public class Enums
+    {
+        public enum PositionType
+        {
+            flat = 0,
+            building = 1,
+            plot = 2
+        }
+    }
+}
---
+++ ShareSphere/Models/InvestmentPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ShareSphere.Models.Enums;
+
+namespace ShareSphere.Models
+{
+    public abstract class InvestmentPosition
+    {
+        public decimal Price { get; set; }
+        public PositionType PositionType {get;set;}
+        public Address Address { get; set; }
+
+    }
+}
